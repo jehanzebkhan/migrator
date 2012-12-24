25a86f8a1c47d9e1c6d1b3c81a5ad3710c5774e1
Jehanzeb Khan <jehanzeb.khan@gmail.com>
2012-12-24 15:20:08 +1100

    Converted it to a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migrator::Application.config.secret_token = 'a250d003d825b1dc1f4ae96c477b086573a5d1a8611e80e0dd7223841a5c78d4b49840ec72d361ae8d1b4f38d0db71291e4805a6e6a54ca75d7b89bd2b8e6fea'
